c0d9b9b0760a6fb973de34b0411faf9aeb35fd0c
Colby T. Ford <colby@tuple.xyz>
2025-06-23 13:58:28 -0400

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,18 @@ from fastmcp import Client
 
 client = Client("server.py")
 
+## Test PDB Method
 async def call_pdb(pdb_id: str):
     async with client:
-        result = await client.call_tool("get_protein_info", {"pdb_id": pdb_id})
+        result = await client.call_tool("get_pdb_info", {"pdb_id": pdb_id})
         print(result)
 
 asyncio.run(call_pdb("5JXE"))
+
+## Test ChEMBL Method
+async def call_chembl(chembl_id: str):
+    async with client:
+        result = await client.call_tool("get_chembl_info", {"chembl_id": chembl_id})
+        print(result)
+
+asyncio.run(call_chembl("CHEMBL25"))
